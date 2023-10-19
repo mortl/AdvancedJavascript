e8f8e3fa71ba0418e28370ab0bda67f7113a3921
Martin Benes <martin.benes66@gmail.com>
2023-10-18 20:29:31 -0400

    updated node

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var _ = require('lodash');
+
+
+console.log(_);
+
+
+var arr = [1,2,3,4,5,6,7,8];
+
+console.log('answer: ', _.without(arr,3));
+
+
+
+
+
+
+
+
 const flattened = [[0,1], [2, 3],[4,5]].reduce(
         (accumulator, array) => {
             //removed debugger;
